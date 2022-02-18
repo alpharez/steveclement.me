3e5f99a8dfc485796e891bf574dbae3eaf5e82b0
Steve Clement <steve.clement@alpharez.com>
2022-02-18 08:59:50 -0500

    editing pcissue

--- pages/posts/pcissue1-1.js
+++ pages/posts/pcissue1-1.js
@@ -18,7 +18,7 @@ export default function Jan2022() {
         Rewind to a couple weeks ago.  I am going about my usual morning business, and usual, I switch on my PC and it flicks on for a moment and then immediately flicks back off.  I built this PC from parts.  I thought, ok this is a power supply that is going bad.
       </p>
       <p>
-      So I get a power supply and plug everything up.  Same issue.
+      So I grab a different 650 watt power supply and plug everything up.  Same issue.
       </p>
       <p>
       So I get a new motherboard and CPU.  Get everything plugged up, apply thermal paste, CPU fan, drives, RAM, etc... You know, everything back together and it powers up to the BIOS.  I mess around with BIOS settings.  I had to change the drive modes around, and I get the OS to boot up.  But same issue happens.  Power on for just a split second and clicks off and nothing I do seems to work.  I unplug the power cable and plug back in and click click.  Leave it for a while, come back, flick the power rocker on the power supply back on, plug power cable in, hit power button and click click.
@@ -67,7 +67,7 @@ export default function Jan2022() {
       <h3>
       Wednesday, the 2nd
       </h3>
-      <p>Bad power supply, or 650 Watts isn't enough all of a sudden.  750 Watt Power Supply seems to work fine now.
+      <p>Bad power supply, or 650 Watts isn't enough all of a sudden.  750 Watt Power Supply seems to work fine now.  Not sure what changed.  The amdgpu linux drivers maybe?
       </p>
      </Layout>
   )
